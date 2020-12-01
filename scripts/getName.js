052e96873c1d8c5fe53a4766038811d6d079d70c
Andrew-Bray <amichaelbray@gmail.com>
2020-12-01 14:03:17 -0800

    getName tests passing with dot notation

--- scripts/getName.js
+++ scripts/getName.js
@@ -1,7 +1,7 @@
 //create a function that returns the property of an object
 
-const getName = ( {name} ) => {
-    return name
+const getName = (object) => {
+    return object.name
 };
 
 module.exports = { getName };
