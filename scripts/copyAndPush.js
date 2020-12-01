25d95886c2a9020559531bd7280b52df5747b103
Andrew-Bray <amichaelbray@gmail.com>
2020-12-01 14:49:07 -0800

    getName test with push and mutate keeps on returning the length of the new array instead of the mutated array

---
+++ scripts/copyAndPush.js
@@ -0,0 +1,7 @@
+
+
+const copyAndPush = (array, num) => {
+   return array.push(num)
+};
+//this is returning the amount of the items in the array... not the content of the array... not certain why
+module.exports = { copyAndPush };
--- scripts/getName.js
+++ scripts/getName.js
@@ -1,7 +1,7 @@
 //create a function that returns the property of an object
 
-const getName = (object) => {
-    return object.name
+const getName = ( {name} ) => {
+    return name
 };
 
 module.exports = { getName };
