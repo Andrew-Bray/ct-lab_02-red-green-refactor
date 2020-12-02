6a11c7178c2d81a4bcba93ed0966e4be1903b6f5
Andrew-Bray <amichaelbray@gmail.com>
2020-12-01 16:29:34 -0800

    capitalizeAndFilter with methods

--- scripts/capitalizeAndFilter.js
+++ scripts/capitalizeAndFilter.js
@@ -1,12 +1,5 @@
 const capitalizeAndFilter = (arr) => {
-    // let noFs = arr.filter(name => !name.startsWith('f'));
-
-    let noFs = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (!arr[i].startsWith('f') && !arr[i].startsWith('F')) {
-noFs.push(arr[i]);
-        }
-    }
+    let noFs = arr.filter(name => !name.startsWith('f'));
 
    const allCaps = noFs.map(name => name.toUpperCase());
     return allCaps;
